8b8f8e69b457c8f487c4d82b8ade37766ac404fc
Blarc <jakob.malezic@gmail.com>
2018-10-25 23:42:07 +0200

    Seminarska_1.3

--- APS/seminarska1/naloga1/src/naloga1.java
+++ APS/seminarska1/naloga1/src/naloga1.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class naloga1 {
 	
-	public static class Oseba {
+	/*public static class Oseba {
 		private int[] cilj = new int[2];
 		private int[] start = new int[2];
 		
@@ -22,21 +22,50 @@ public class naloga1 {
 		public int[] getStart() {
 			return start;
 		}
-	}
+	}*/
 	
-	public int abs(int n) {
+	public static int abs(int n) {
 		if (n < 0) {
 			return -n;
 		}
 		return n;
 	}
-	public int razdalja(int[] a, int[] b) {
+	public static int razdalja(int[] a, int[] b) {
 		return abs(a[0] - b[0]) + abs(a[1] - b[1]);
 	}
 	
-	public int fun() {
+	public static int fun(int[] taxi, int[][] starti, int[][] cilji, int left, int razdalja, int m) {
+		if (left == 0) {
+			return razdalja;
+		}
+		int[] oTaxi = taxi;
+		int[][] oStarti = starti;
+		int oLeft = left;
+		int oRazdalja = razdalja;
+		
+		
+		for (int i = 0; i < m; i++) {
+			if (starti[i] != null) {
+				razdalja += razdalja(taxi, starti[i]);
+				taxi = starti[i];
+				
+				if (cilji[i] == starti[i]) {
+					left--;
+					starti[i] = null;
+					System.out.println(fun(taxi, starti, cilji, left, razdalja, m));
+					
+				} else {
+					starti[i] = cilji[i];
+					System.out.println(fun(taxi, starti, cilji, left, razdalja, m));
+				}
+				System.out.println(fun(oTaxi, oStarti, cilji, oLeft, oRazdalja, m));
+				
+			}
+			
+		}
 		return 0;
 	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -50,24 +79,37 @@ public class naloga1 {
 		String[] line;
 		int n = Integer.parseInt((br.readLine().split(""))[0]);
 		line = br.readLine().split(",");
-		int xTaxi = Integer.parseInt(line[0]);
-		int yTaxi = Integer.parseInt(line[0]);
+		int[] taxi = {Integer.parseInt(line[0]), Integer.parseInt(line[1])} ;
 		int m = Integer.parseInt((br.readLine().split(""))[0]);
 		
-		String[][] strankeNizi = new String[m][5];
-		Oseba[] stranke = new Oseba[m];
+		int[][] starti = new int[m][2];
+		int[][] cilji = new int[m][2];
 		
-		for (int i = 0; i < m; i++) {
+		String[][] strankeNizi = new String[m][5];
+		
+		//Oseba[] stranke = new Oseba[m];
+		
+		/*for (int i = 0; i < m; i++) {
 			strankeNizi[i] = br.readLine().split(",");
 			stranke[i] = new Oseba(Integer.parseInt(strankeNizi[i][1]),
 								   Integer.parseInt(strankeNizi[i][2]),
 								   Integer.parseInt(strankeNizi[i][3]),
 								   Integer.parseInt(strankeNizi[i][4]));
+		}*/
+		
+
+		for (int i = 0; i < m; i++) {
+			strankeNizi[i] = br.readLine().split(",");
+			starti[i][0] = Integer.parseInt(strankeNizi[i][1]);
+			starti[i][1] = Integer.parseInt(strankeNizi[i][2]);
+			cilji[i][0] = Integer.parseInt(strankeNizi[i][3]);
+			cilji[i][1] = Integer.parseInt(strankeNizi[i][4]);
 		}
 		
+		fun(taxi, starti, cilji, m, 0, m);
 		
 		
-		
+		br.close();
 	}
 
 }
