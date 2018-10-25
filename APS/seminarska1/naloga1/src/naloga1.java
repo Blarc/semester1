cca40da37bf46b9f6fecac89308fc3740e27654d
Blarc <jakob.malezic@gmail.com>
2018-10-25 11:16:36 +0200

    seminarska1.2

--- APS/seminarska1/naloga1/src/naloga1.java
+++ APS/seminarska1/naloga1/src/naloga1.java
@@ -2,43 +2,72 @@ import java.io.*;
 import java.util.Arrays;
 
 public class naloga1 {
-
+	
+	public static class Oseba {
+		private int[] cilj = new int[2];
+		private int[] start = new int[2];
+		
+		public Oseba(int xStart, int yStart, int xCilj, int yCilj) {
+			this.start[0] = xStart;
+			this.start[1] = yStart;
+			this.cilj[0] = xCilj;
+			this.cilj[1] = yCilj;
+			
+		}
+		
+		public int[] getCilj() {
+			return cilj;
+		}
+		
+		public int[] getStart() {
+			return start;
+		}
+	}
+	
+	public int abs(int n) {
+		if (n < 0) {
+			return -n;
+		}
+		return n;
+	}
+	public int razdalja(int[] a, int[] b) {
+		return abs(a[0] - b[0]) + abs(a[1] - b[1]);
+	}
+	
+	public int fun() {
+		return 0;
+	}
+	
 	public static void main(String[] args) throws IOException {
 		
 		if(args.length < 1) {
 			System.out.println("Uporaba: java naloga1 <podatki> <resitev>");
 			System.exit(1);
 		}
+		
 		BufferedReader br = new BufferedReader(new FileReader(args[0]));
 		
-		/*int n = br.read() -'0';
-		br.read();
-		br.read();
-		int xStart = br.read() -'0';
-		br.read();
-		int yStart = br.read() -'0';
-		br.read();
-		br.read();
-		int m = br.read() -'0';
-		br.read();
-		br.read();
-		int[][] stranke = new int[m][5];
+		String[] line;
+		int n = Integer.parseInt((br.readLine().split(""))[0]);
+		line = br.readLine().split(",");
+		int xTaxi = Integer.parseInt(line[0]);
+		int yTaxi = Integer.parseInt(line[0]);
+		int m = Integer.parseInt((br.readLine().split(""))[0]);
 		
-		System.out.printf("n: %d x: %d y: %d m: %d\n", n, xStart, yStart, m);
+		String[][] strankeNizi = new String[m][5];
+		Oseba[] stranke = new Oseba[m];
 		
 		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < 5; j++) {
-				stranke[i][j] = br.read()-'0';
-				br.read();
-			}
-			br.read();
+			strankeNizi[i] = br.readLine().split(",");
+			stranke[i] = new Oseba(Integer.parseInt(strankeNizi[i][1]),
+								   Integer.parseInt(strankeNizi[i][2]),
+								   Integer.parseInt(strankeNizi[i][3]),
+								   Integer.parseInt(strankeNizi[i][4]));
 		}
 		
-		System.out.print(Arrays.deepToString(stranke));
-		*/
-		String line;
-		int n = ((br.readLine()).charAt(0)-'0');
-		System.out.println(n);
+		
+		
+		
 	}
 
 }
