c70ec34310cb3bd074d694c829a95429735f2718
Blarc <jakob.malezic@gmail.com>
2018-11-04 23:14:21 +0100

    seminarska2.2; naloga 2 works

--- APS/seminarska1/naloga2/src/naloga2.java
+++ APS/seminarska1/naloga2/src/naloga2.java
@@ -1,13 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class naloga2 {
 
     public static class Malloc {
+        int vseh = 0;
         int[][] tab;
         int[] idTab;
         int length;
         int atm = 0;
-        int n = 0;
 
         public void init(int size) {
             this.length = size;
@@ -16,56 +19,185 @@ public class naloga2 {
             this.tab[0][1] = size;
         }
 
+
+        //v zadnjo se zapiše oboje
         public boolean alloc(int size, int id) {
-            for (int i = atm; i <= n; i++) {
-                if (tab[i][1] >= size && tab[i][0] == 0) {
-                    idTab[id-1] = i;
-                    tab[i][0] = id;
-                    tab[i][1] = size;
-                    tab[i+1][1] = length - tab[i][1];
-                    length -= tab[i][1];
-                    this.atm++;
-                    this.n++;
-                    return true;
+            if (idTab[id-1] == 0) {
+                for (int i = atm; i < length; i += tab[i][1]) {
+                    if (tab[i][1] >= size && tab[i][0] == 0) {
+                        this.vseh++;
+                        idTab[id - 1] = i + 1;
+                        if (i + size < length && tab[i + size][0] == 0) {
+                            tab[i + size][1] = tab[i][1] - size;
+                            tab[i + tab[i][1] - 1][1] = tab[i][1] - size;
+                        }
+                        tab[i][0] = id;
+                        tab[i][1] = size;
+                        tab[i + size - 1][0] = id;
+                        if (atm + size < length && atm == i) {
+                            atm += size;
+                        }
+                        return true;
+                    }
                 }
             }
 
             return false;
         }
 
+        // THIS WORKS !!!
         public int free(int id) {
-            this.atm = idTab[id-1];
-            int temp = tab[idTab[id-1]][1];
-            tab[idTab[id-1]][0] = 0;
-            idTab[id-1] = 0;
-            return temp;
+            if (idTab[id-1] > 0) {
+                if (idTab[id - 1] - 1 < atm) {
+                    atm = idTab[id - 1] - 1;
+                }
+
+                int mta = idTab[id - 1] - 1;
+                int res = tab[mta][1];
+                int why = tab[mta][1];
+                int next = tab[mta][1];
+
+                if (mta + next < length && tab[mta + next][0] == 0) {
+                    tab[mta + next - 1][0] = 0;
+                    int temp = next;
+
+                    res += tab[mta + next][1];
+                    next += tab[mta + next][1];
+
+                    tab[mta + temp][1] = 0;
+                }
+
+                if (mta - 1 >= 0 && tab[mta - 1][0] == 0) {
+                    next += tab[mta - 1][1];
+                    res += tab[mta - 1][1];
+
+                    tab[mta][0] = 0;
+
+                    tab[mta][1] = 0;
+                    int temp = tab[mta - 1][1];
+                    tab[mta - 1][1] = 0;
+
+                    mta -= temp;
+                }
+
+                tab[mta][0] = 0;
+                tab[mta][1] = res;
+                tab[mta + next - 1][0] = 0;
+                tab[mta + next - 1][1] = res;
+                idTab[id - 1] = 0;
+
+                return why;
+            }
+            return 0;
         }
 
+        //CLEAN UP AWAITS
         public void defrag(int a) {
-            for (int i = 0; i < this.n; i++) {
-                if (tab[i][0] == 0) {
-                    int[] temp = tab[i];
-                    tab[i] = tab[i+1];
-                    tab[i+1] = temp;
-                    idTab[tab[i][0]-1] = i;
-                    break;
+            for (int j = 0; j < a; j++) {
+                while(tab[atm][0] != 0) {
+                    atm += tab[atm][1];
                 }
+                int next = tab[atm][1];
+                int temp = next;
+
+                tab[atm][0] = tab[atm + next][0];
+                tab[atm][1] = tab[atm + next][1];
+                tab[atm + next][0] = 0;
+                tab[atm + next][1] = 0;
+                int g = tab[atm][1];
+                tab[atm + g - 1][0] = tab[atm][0];
+
+                tab[atm + next - 1][1] = 0;
+                tab[atm + g + next - 1][0] = 0;
+
+                if (atm + g + next < length && tab[atm + g + next][0] == 0) {
+                    next += tab[atm + g + next][1];
+                    tab[atm + g + temp][1] = 0;
+                }
+
+                tab[atm][1] = g;
+                tab[atm + g][1] = next;
+                tab[atm + g + next - 1][1] = next;
+                idTab[tab[atm][0] - 1] -= temp;
             }
         }
+
+        /*public void defrag(int a) {
+            for (int i = 0; i < a; i++) {
+                while(tab[atm][0] != 0) {
+                    atm += tab[atm][1];
+                }
+                int next = tab[atm][1];
+                int id = tab[atm + next][0];
+                int size = tab[atm + next][1];
+                free(id);
+                alloc(size, id);
+            }
+        }*/
     }
 
-    public static void main(String[] args) {
-        Malloc nov = new Malloc();
-        nov.init(15);
-        nov.alloc(5,1);
-        nov.alloc(3,2);
-        nov.alloc(2,3);
-        nov.free(2);
-        nov.alloc(4, 4);
-        nov.defrag(1);
-        nov.alloc(2, 5);
+    public static void main(String[] args) throws IOException{
 
-        System.out.println(Arrays.toString(nov.idTab));
-        System.out.println(Arrays.deepToString(nov.tab));
+        if(args.length < 0) {
+            System.out.println("Uporaba: java naloga2 <vhodna datoteka> <izhodna datoteka>");
+            System.exit(1);
+        }
+
+        BufferedReader br = new BufferedReader(new FileReader(args[0]));
+        String[] line;
+        line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        System.out.println("n: " + n);
+
+
+        long startTime = System.currentTimeMillis();
+        Malloc plswork = new Malloc();
+        for (int i = 0; i < n; i++) {
+            line = br.readLine().split(",");
+            String ukaz = line[0];
+            int bard = Integer.parseInt(line[1]);
+            switch(ukaz) {
+                case "i":
+                    //System.out.println(bard);
+                    plswork.init(bard);
+                    break;
+                case "a":
+                    int sion = Integer.parseInt(line[2]);
+                    //System.out.println(bard + " " + sion);
+                    plswork.alloc(bard, sion);
+                    break;
+                case "f":
+                    //System.out.println(bard);
+                    plswork.free(bard);
+                    break;
+                case "d":
+                    //System.out.println(bard);
+                    plswork.defrag(bard);
+                    break;
+                    default: System.out.println("invalid");
+            }
+            if (i == n-1) {
+                System.out.println(Arrays.toString(line));
+            }
+        }
+
+
+        int[][] tab = plswork.tab;
+        for (int i = 0; i < plswork.length; i += tab[i][1]) {
+            int id = tab[i][0];
+            int start = i;
+            int end = start + tab[i][1] - 1;
+            if (tab[i][0] != 0) {
+                System.out.println(id + "," + start + "," + end);
+            }
+
+        }
+
+        //System.out.println("ATM: " + plswork.atm);
+        //System.out.println(Arrays.toString(plswork.idTab));
+        //System.out.println(Arrays.deepToString(plswork.tab));
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println("Elapsed Time: " + elapsedTime + " ms");
     }
 }
