1e878a4414dd63a50a2999d0786a4c96ab673985
Blarc <jakob.malezic@gmail.com>
2018-12-18 11:32:25 +0100

    zacetek dela na seminarski2

---
+++ APS/seminarska2/naloga6/src/Naloga6_ver1.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Naloga6_ver1 {
+
+	public static void main(String[] args) throws IOException {
+		long startTime = System.currentTimeMillis();
+		
+		if(args.length < 1) {
+			System.out.println("Uporaba: java naloga1 <podatki> <resitev>");
+			System.exit(1);
+		}
+		
+		BufferedReader br = new BufferedReader(new FileReader(args[0]));
+	
+		br.close();
+		
+		//PrintWriter writer = new PrintWriter(new FileWriter(args[1]));
+		//writer.close();
+		
+		long stopTime = System.currentTimeMillis();
+	    long elapsedTime = stopTime - startTime;
+	    System.out.println("Elapsed time: " + elapsedTime + " ms");
+	}
+
+}
